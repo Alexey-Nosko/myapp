d774eaf78d569cc9add7bce7d56fea9009f92293
Alexey-Nosko <alexeynosko17@gmail.com>
2025-02-15 15:35:27 +0300

    Initial commit

---
+++ src/main/java/com/example/myapp/controllers/Controller.java
@@ -0,0 +1,15 @@
+package com.example.myapp.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hand")
+public class Controller {
+
+    @GetMapping
+    public String hello() {
+        return "Hello world Version 1";
+    }
+}
