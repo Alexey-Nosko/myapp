47a5017a650a238dab7c39a610bd18706fc3bbd5
Alexey-Nosko <alexeynosko17@gmail.com>
2025-02-18 13:49:06 +0300

    version 34

--- src/main/java/com/example/myapp/MyappApplication.java
+++ src/main/java/com/example/myapp/MyappApplication.java
@@ -2,6 +2,7 @@ package com.example.myapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
@@ -11,6 +12,8 @@ public class MyappApplication extends SpringBootServletInitializer {
 		SpringApplication.run(MyappApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(MyappApplication.class);
+	}
 }
-
-
--- src/main/java/com/example/myapp/controllers/Controller.java
+++ src/main/java/com/example/myapp/controllers/Controller.java
@@ -10,6 +10,6 @@ public class Controller {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello world Version 33";
+        return "Hello world Version 34";
     }
 }
